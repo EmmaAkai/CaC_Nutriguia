7a2fba5495dda76753480afc137909c556f428f9
Emma_Akai <EmmaAkaiGit@gmail.com>
2023-10-23 22:11:58 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+class recipe {
+    constructor(title, description,instructions) {
+        this.title = title;
+        this.description = description;
+        this.instructions = instructions;
+    }
+}
+
+let recipe = new recipe('Milanesa con pure',"milanesas con pure super ricas","Añadir las milanesas y despues el pure");
