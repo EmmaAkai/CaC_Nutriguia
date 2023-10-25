6dc1d2854c115e545f8c7008f95b740b092eac0c
Emma_Akai <EmmaAkaiGit@gmail.com>
2023-10-25 17:24:48 -0300

    Se agrego parte del fetch de la api

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,20 @@
-const fetchRecipes = () => {
-    fetch();
-
+let params = '';
+let urlBase = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com';
+let recipeSearch = '/recipes/complexSearch?query=';
+let options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'SING-UP-FOR-KEY',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapid'
+    }
 }
+
+let url = urlBase + recipeSearch + params;
+
+let response = await fetch(url, options);
+
+if (response.ok) {
+    let result = await response.text();
+    console.log(result);
+}
+
