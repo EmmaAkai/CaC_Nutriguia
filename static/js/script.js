9970dcf986e6747855f7d3ee8fe29182a356c2c4
Emma_Akai <EmmaAkaiGit@gmail.com>
2023-10-26 16:55:00 -0300

    Se agrego la API y se termino la pagina, por lo menos mi parte

--- static/js/script.js
+++ static/js/script.js
@@ -7,14 +7,47 @@ const options = {
 	}   
 };
 
-async function ok () {
-    try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
+const fetchRecipes = () => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(response =>{
+            console.log(response);
+            let recipes = response.d;
+            let imgContainer = document.querySelector('#imgContainer');
+            for (let i = 0; i < 9; i++) {
+                let html = `
+                    <li><a href="" ><img src="${recipes[i].Image}" alt=""></a></li>
+                `;
+                imgContainer.insertAdjacentHTML('beforeend',html);
+            }
+            let popularContainer = document.querySelector('#popularContainer');
+            for(let i = 9; i<14; i++) {
+                let html = `
+                    <li>
+                        <a href="/recipe.html"><img src="${recipes[i].Image}" alt=""></a>
+                    </li>
+                `;
+                popularContainer.insertAdjacentHTML('beforeend',html);
+            }
+            let recientesContainer = document.querySelector('#recientesContainer');
+            for(let i = 14; i<24; i++){
+                let html = `
+                    <li class="item-container">
+                        <div class="content-container">
+                            <a href="">
+                                <img src="${recipes[i].Image}" alt="">
+                                <h3>${recipes[i].Title}</h3>
+                        </a>
+                        </div>
+                    </li>
+                `;
+                recientesContainer.insertAdjacentHTML('beforeend',html);
+            }
+        })
+        .catch(err => console.error(err));
 
- ok()
+};
+
+console.log('Llamada a la api');
+fetchRecipes();
+
